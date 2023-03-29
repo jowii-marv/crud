33aef5619abd619752392f2313a55d0ee3554a04
Marv <malvin.opondo@student.moringaschool.com>
2023-03-29 22:09:14 +0300

    feat(Components):Create an employee database

---
+++ src/components/Employee.js
@@ -0,0 +1,24 @@
+
+
+const Employees = [
+    {
+        id: "1",
+        Name: "Japheth Nyaranga",
+        Age: "38",
+    },
+    {
+        id: "2",
+        Name:"Malvin Opondo",
+        Age: "39",
+    },
+    {
+        id: "3",
+        Name:"Charles Akendo",
+        Age:"36"
+    },
+    {
+        id: "4",
+        Name:"Vincent Ouma",
+        Age: "24"
+    }
+]
